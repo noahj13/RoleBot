9cf4843597cf6505671e261ef4032446f96fdcff
noahj13 <noahjarvis0@gmail.com>
2019-08-31 13:54:04 -0400

    RoleBot now scans a given channel in a given server for role to assign or remove, any other message in that channel is deleted

---
+++ src/main/java/RoleBot.java
@@ -0,0 +1,71 @@
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.channel.Channel;
+import org.javacord.api.entity.permission.Permissions;
+import org.javacord.api.entity.permission.Role;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.entity.user.User;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RoleBot {
+    public static void main(String[] args) {
+        // Insert your bot's token here
+        String token = "";
+        String channelIDT = "";
+        String serverIDT = "";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File("src/main/resources/token.cred")));
+            token = reader.readLine();
+            channelIDT = reader.readLine();
+            serverIDT = reader.readLine();
+        }catch (IOException e){
+            System.out.println("could not find token");
+            System.exit(1);
+        }
+        final String channelID = channelIDT;
+        final String serverID = serverIDT;
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+        Server server = api.getServerById(serverID).get();
+        //Channel ch = api.getChannelById(channelID).get();
+
+        api.addMessageCreateListener(event -> {
+            List<Role> roles = server.getRoles();
+            if(event.getChannel().getIdAsString().equals(channelID)){
+                String message = event.getMessageContent();
+                if(message.equals("@Everyone")||message.equals("RoleBot")){
+                    event.getMessage().delete();
+                }
+                else if (event.getMessageAuthor().asUser().isPresent()) {
+                    User user = event.getMessageAuthor().asUser().get();
+                    boolean found = false;
+                    for(Role r:roles){
+                        if(r.getName().equals(message)){
+                            user.addRole(r);
+                            found = true;
+                        }
+                    }
+                    if(!found){
+                        event.getMessage().delete();
+                    }
+                }
+            }
+        });
+        api.addMessageDeleteListener(event -> {
+            List<Role> roles = server.getRoles();
+            if(event.getChannel().getIdAsString().equals(channelID)){
+                String message = event.getMessageContent().orElse("");
+                if (event.getMessageAuthor().isPresent()) {
+                    User user = event.getMessageAuthor().get().asUser().get();
+                    roles.parallelStream().filter(r -> r.getName().equals(message)).forEach(user::removeRole);
+                }
+            }
+        });
+
+        // Print the invite url of your bot
+        System.out.println("You can invite the bot by using the following url: " + api.createBotInvite(Permissions.fromBitmask(268443648)));
+    }
+}
